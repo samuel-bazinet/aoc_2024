5cadc33a773170bc357f80b1cc01a36a4ef6d11a
samuel-bazinet <bazinet.samuel1@gmail.com>
2024-12-01 00:38:37 -0500

    Completed day 1

--- day1/part_1.py
+++ day1/part_1.py
@@ -1,3 +1,23 @@
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+
+pairs = [a.strip().split('   ') for a in lines]
+print(pairs)
+list_a = []
+list_b = []
+for e in pairs:
+    list_a.append(int(e[0]))
+    list_b.append(int(e[1]))
+
+list_a.sort()
+list_b.sort()
+print(list_a)
+print(list_b)
+
+out = 0
+
+for (l, r) in zip(list_a, list_b):
+    out += abs(l - r)
+
+print(out)
--- day1/part_2.py
+++ day1/part_2.py
@@ -1,3 +1,27 @@
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+
+pairs = [a.strip().split('   ') for a in lines]
+print(pairs)
+list_a = []
+list_b = []
+for e in pairs:
+    list_a.append(int(e[0]))
+    list_b.append(int(e[1]))
+
+list_a.sort()
+list_b.sort()
+print(list_a)
+print(list_b)
+
+out = 0
+
+for i in list_a:
+    count = 0
+    for j in list_b:
+        if i == j:
+            count += 1
+    out += count*i
+
+print(out)
