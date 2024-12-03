87128930bc5337ff2ffad85356c44bee3cb4307f
samuel-bazinet <bazinet.samuel1@gmail.com>
2024-12-03 00:19:49 -0500

    Completed day 3

--- day3/part_1.py
+++ day3/part_1.py
@@ -1,3 +1,15 @@
+import re 
+from functools import reduce
 
 with open('input.txt', 'r') as file:
-    lines = file.readlines()
+    file_content = file.read()
+
+matches = re.findall('mul\(\d{1,3},\d{1,3}\)', file_content)
+
+result = 0
+
+for mul in matches:
+    content = mul[4:-1]
+    result += reduce(lambda x, y: x*y, (int(i) for i in content.split(',')))
+
+print(result)
--- day3/part_2.py
+++ day3/part_2.py
@@ -1,3 +1,23 @@
+import re 
+from functools import reduce
 
 with open('input.txt', 'r') as file:
-    lines = file.readlines()
+    file_content = file.read()
+
+matches = re.finditer("mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", file_content)
+
+result = 0
+on = True
+
+for mul in matches:
+    if mul.group() == 'do()':
+        print(mul)
+        on = True
+    elif mul.group() == "don't()":
+        print(mul)
+        on = False
+    elif on:
+        content = mul.group()[4:-1]
+        result += reduce(lambda x, y: x*y, (int(i) for i in content.split(',')))
+
+print(result)
