e5a0455ebe7592c36de6469f062cdde52e64587d
samuel-bazinet <bazinet.samuel1@gmail.com>
2024-12-07 00:26:05 -0500

    Completed day 7

--- day7/part_1.py
+++ day7/part_1.py
@@ -1,3 +1,33 @@
+from itertools import product
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+
+OPERATORS = ['+', '*']
+
+def verify_ops(result: int, operands: list[int]) -> bool:
+    opss = product(OPERATORS, repeat=len(operands)-1)
+    for ops in opss:
+        v = operands[0]
+        for i in range(len(operands)-1):
+            if ops[i] == '+':
+                v += operands[i+1]
+            else:
+                v *= operands[i+1]
+        if v == result:
+            return True
+    return False 
+        
+
+        
+
+out = 0
+
+for line in lines:
+    line = line.strip().split(':')
+    result = int(line[0])
+    operands = [int(a) for a in line[1].strip().split(' ')]
+    if verify_ops(result, operands):
+        out += result
+
+print(out)
