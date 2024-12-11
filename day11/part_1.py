fb48e41f9af473f2e3a9246c15c83748dcf51053
samuel-bazinet <bazinet.samuel1@gmail.com>
2024-12-11 01:32:45 -0500

    Completed day 11

--- day11/part_1.py
+++ day11/part_1.py
@@ -1,3 +1,26 @@
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+
+stones = lines[0].strip().split()
+
+# rules:
+# 0 -> 1
+# even digits -> split 2
+# multiply by 2024
+
+def apply_rules(stone: str) -> list[str]:
+    if stone == '0':
+        return ['1']
+    if len(stone) % 2 == 0:
+        return [stone[:len(stone)//2], str(int(stone[len(stone)//2:]))]
+    return [str(int(stone)*2024)]
+
+for i in range(25):
+    new_stones = []
+    for stone in stones:
+        new_stones.extend(apply_rules(stone))
+    print(len(stones), len(new_stones))
+    stones = new_stones
+
+print(len(stones))
--- day11/part_2.py
+++ day11/part_2.py
@@ -1,3 +1,29 @@
-
+from math import exp, ceil
+import numpy as np
+from functools import cache
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+
+stones = lines[0].strip().split()
+
+# rules:
+# 0 -> 1
+# even digits -> split 2
+# multiply by 2024
+
+@cache
+def apply_rules(stone: str, iter: int) -> int:
+    if iter == 0:
+        return 1
+    if stone == '0':
+        return apply_rules('1', iter-1)
+    if len(stone) % 2 == 0:
+        return apply_rules(stone[:len(stone)//2], iter-1) + apply_rules(str(int(stone[len(stone)//2:])), iter-1)
+    return apply_rules(str(int(stone)*2024), iter - 1)
+
+result= 0
+
+for stone in stones:
+    result += apply_rules(stone, 75)
+
+print(result)
