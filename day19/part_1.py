da5fa826ed2e69eb275a3e33732a1061c553cc15
samuel-bazinet <bazinet.samuel1@gmail.com>
2024-12-19 00:25:25 -0500

    Completed day 19

--- day19/part_1.py
+++ day19/part_1.py
@@ -1,3 +1,28 @@
+from functools import cache
+
+@cache
+def contains(to_check: str) -> bool:
+    ret_val = False
+    for t in available:
+        if to_check.startswith(t):
+            if len(t) == len(to_check):
+                return True
+            else:
+                ret_val = ret_val or contains(to_check[len(t):]) 
+    return ret_val
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+
+available = lines[0].strip().split(', ')
+
+to_check = [line.strip() for line in lines[2:]]
+
+result = 0
+
+for t in to_check:
+    if contains(t):
+        #print(t, 'good')
+        result += 1
+
+print(result)
--- day19/part_2.py
+++ day19/part_2.py
@@ -1,3 +1,29 @@
+from functools import cache
+
+@cache
+def contains(to_check: str) -> int:
+    ret_val = 0
+    if len(to_check) == 0:
+        return 1
+    for t in available:
+        if to_check.startswith(t):
+            #print(':', to_check)
+            #print(t)
+            ret_val += contains(to_check[len(t):]) 
+    return ret_val
 
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+
+available = lines[0].strip().split(', ')
+
+to_check = [line.strip() for line in lines[2:]]
+
+result = 0
+
+for t in to_check:
+    r = contains(t)
+    #print(r, '\n')
+    result += r
+
+print(result)
