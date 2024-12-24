cac51ee60258f767a80381bd67fb06835f874b28
samuel-bazinet <bazinet.samuel1@gmail.com>
2024-12-23 23:52:15 -0500

    Completed day 23

--- day23/part_1.py
+++ day23/part_1.py
@@ -1,3 +1,41 @@
 
+
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+neighs: dict[str, list[str]] = {}
+
+for line in lines:
+    line = line.strip().split('-')
+    l = line[0]
+    r = line[1]
+    if l not in neighs.keys():
+        neighs[l] = [r]
+    else:
+        neighs[l].append(r)
+    if r not in neighs.keys():
+        neighs[r] = [l]
+    else:
+        neighs[r].append(l)
+
+trips: list[set[str]] = []
+
+for k, v in neighs.items():
+    for n in v:
+        for n2 in neighs[n]:
+            if n2 in v:
+                if {k, n, n2} not in trips:
+                    trips.append({k,n,n2})
+print(len(trips))
+
+trimmed = []
+
+for trip in trips:
+    g = False
+    for c in trip:
+        if c[0] == 't':
+            g = True
+
+    if g:
+        trimmed.append(trip)
+
+print(len(trimmed))
--- day23/part_2.py
+++ day23/part_2.py
@@ -1,3 +1,39 @@
 
+
 with open('input.txt', 'r') as file:
     lines = file.readlines()
+neighs: dict[str, list[str]] = {}
+
+for line in lines:
+    line = line.strip().split('-')
+    l = line[0]
+    r = line[1]
+    if l not in neighs.keys():
+        neighs[l] = [r]
+    else:
+        neighs[l].append(r)
+    if r not in neighs.keys():
+        neighs[r] = [l]
+    else:
+        neighs[r].append(l)
+
+trips: list[set[str]] = []
+
+for k, v in neighs.items():
+    current_trip = {k}
+    for n in v:
+        to_add = True
+        for n2 in current_trip:
+            if n2 not in neighs[n]:
+                to_add = False
+                break
+        if to_add:
+            current_trip.add(n)
+    if current_trip not in trips:
+        trips.append(current_trip)
+        
+lengths: list[int] = [len(t) for t in trips]
+l_i = lengths.index(max(lengths))
+l_t = list(trips[l_i])
+
+print(','.join(sorted(l_t)))
